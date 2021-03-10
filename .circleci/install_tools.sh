ab216f41239e57eda2e6e4572ec196acd2bef846
Pierre Tessier <pierre@pierretessier.com>
2021-03-10 12:44:16 -0500

    fix chart releaser version

--- .circleci/install_tools.sh
+++ .circleci/install_tools.sh
@@ -13,8 +13,8 @@ sudo ln -s "/usr/local/helm-v$HELM_VERSION/linux-amd64/helm" /usr/local/bin/helm
 rm -f "helm-v$HELM_VERSION-linux-amd64.tar.gz"
 
 echo "Installing chart-releaser..."
-curl -LO "https://github.com/helm/chart-releaser/releases/download/v${CHART_RELEASER_VERSION}/chart-releaser_${CHART_RELEASER_VERSION}_Linux_x86_64.tar.gz"
+curl -LO "https://github.com/helm/chart-releaser/releases/download/v${CHART_RELEASER_VERSION}/chart-releaser_${CHART_RELEASER_VERSION}_linux_amd64.tar.gz"
 sudo mkdir -p "/usr/local/chart-releaser-v$CHART_RELEASER_VERSION"
-sudo tar -xzf "chart-releaser_${CHART_RELEASER_VERSION}_Linux_x86_64.tar.gz" -C "/usr/local/chart-releaser-v$CHART_RELEASER_VERSION"
+sudo tar -xzf "chart-releaser_${CHART_RELEASER_VERSION}_linux_amd64.tar.gz" -C "/usr/local/chart-releaser-v$CHART_RELEASER_VERSION"
 sudo ln -s "/usr/local/chart-releaser-v$CHART_RELEASER_VERSION/chart-releaser" /usr/local/bin/chart-releaser
-rm -f "chart-releaser_${CHART_RELEASER_VERSION}_Linux_x86_64.tar.gz"
+rm -f "chart-releaser_${CHART_RELEASER_VERSION}_linux_amd64.tar.gz"
